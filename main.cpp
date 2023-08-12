5671454916947dea938f5c044ee5176f0a097473
Swyrias <diegofgf34@gmail.com>
2023-08-12 00:16:51 -0300

    saving updated main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "clockType.h"
 
 int main() {
-	std::cout << "Hello world" << endl;
+	clockType clockOne(10, 45, 33);
+	clockOne.print();
+	std::cout << endl << "Hello world" << endl;
 	return 0;
 }
